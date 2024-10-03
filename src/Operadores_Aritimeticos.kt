4de4c25ed837912175caa0dd0999fea09494a66b
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2024-10-03 20:41:32 -0300

    update

---
+++ src/Operadores_Aritimeticos.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var numero1 = 10
+    var numero2 = 5
+
+    val resultado = numero1 + numero2
+
+    print(resultado)
+}
---
+++ src/Variveis_e_Constantes.kt
@@ -0,0 +1,15 @@
+const val name1 = "Matheus"
+
+fun main() {
+    // Sem tipo indefinido - Imutavel
+    val name = "Matheus"
+
+    // Com tipo definido - Imutavel
+    val name2:String = "Matheus"
+
+    // Com tipo indefinido - Mutavel
+    var name3 = "Rodrigues"
+
+
+    print(name)
+}
